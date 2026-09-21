fdbe75472c3c9b2fd45e2e92ee9edb8c68257fc4
belokonme <belokon.me@gmail.com>
2026-09-21 17:53:03 +0300

    Create calculator.py, add 'main'

---
+++ HW2_Belokon/calculator.py
@@ -0,0 +1,27 @@
+
+   
+
+
+ 
+
+
+    
+
+
+    
+
+def main(a,b):
+    a = float(input("First number: "))
+    op = input("Operation: ")
+    b = float(input("Second number: "))
+
+    if op == "+":
+        print(add(a,b))
+    elif op == "-":
+        print(substract(a,b))
+    elif op == "/":
+        print(divide(a,b))
+    elif op == "*":
+        print(multiply(a,b))
+
+main(a,b)
